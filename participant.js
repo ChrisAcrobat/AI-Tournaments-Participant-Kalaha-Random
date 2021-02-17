887dfb3de3045e2f73c741ecc9753db89f8fb1a3
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-02-17 08:53:53 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,13 +1,9 @@
 'use strict'
-function tock(messageEvent){
-	let length = (messageEvent.data.length/2) - 2;
+ParticipantHelper.onmessage = data => {
+	let length = (data.length/2) - 2;
 	let index;
 	do{
 		index = Math.round(Math.random()*length);
-	}while(0 == messageEvent.data[index]);
-	postMessage(index);
+	}while(0 == data[index]);
+	ParticipantHelper.respond(index);
 }
-onmessage = messageEvent => {
-	onmessage = tock;
-	postMessage(null);
-};
